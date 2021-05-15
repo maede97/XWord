f0889cf8ee00198be0377b427ce26a2012c0c60f
Matthias Busenhart <busenham@student.ethz.ch>
2021-05-15 15:44:24 +0200

    Make server variables shared (for concurrent requests), run formatter

--- server.py
+++ server.py
@@ -1,79 +1,91 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from multiprocessing import Value, Array
+from ctypes import c_wchar_p
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'CHANGE_THIS'
 
 socketio = SocketIO(app)
 
-online_users = 0
+online_users = Value('i', 0)
 puzzle_size = 15
 
-board = " " * puzzle_size * puzzle_size
-numbers = [0] * puzzle_size * puzzle_size
-circles = [0] * puzzle_size * puzzle_size
+board = Value(c_wchar_p, " " * puzzle_size * puzzle_size)
+numbers = Array('i', [0] * puzzle_size * puzzle_size)
+circles = Array('i', [0] * puzzle_size * puzzle_size)
 
-# Routes
+
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @socketio.on('get-online-people')
 def handleGetOnlinePeople():
     # send online people to all users
-    emit('get-online-people', {'online-users': online_users}, broadcast=True)
+    with online_users.get_lock():
+        emit('get-online-people',
+             {'online-users': online_users.value}, broadcast=True)
+
 
 @socketio.on('connection')
 def handleConnection(_=None):
-    global online_users
-    online_users+=1
-    sendPuzzleSize()
-    handleGetOnlinePeople()
+    with online_users.get_lock():
+        online_users.value += 1
+        sendPuzzleSize()
+        handleGetOnlinePeople()
+
 
 @socketio.on('disconnect')
 def handleDisconnect(_=None):
-    global online_users
-    online_users-=1
-    handleGetOnlinePeople()
+    with online_users.get_lock():
+        online_users.value -= 1
+        handleGetOnlinePeople()
+
 
 @socketio.on("puzzlesize")
 def sendPuzzleSize():
-    emit("puzzlesize", {"size":puzzle_size})
+    emit("puzzlesize", {"size": puzzle_size})
+
 
 @socketio.on("board")
 def sendBoard():
-    emit("board", {"board": board, "numbers": numbers, "circles": circles})
+    with board.get_lock(), numbers.get_lock(), circles.get_lock():
+        emit("board", {"board": board.value,
+             "numbers": numbers.value, "circles": circles.value})
+
 
 @socketio.on("update-board")
 def updateBoard(data):
-    global board
-    global numbers
-    global circles
-    board = data["board"]
-    numbers = data["numbers"]
-    circles = data["circles"]
-    emit("board", {"board": board, "numbers": numbers, "circles": circles}, broadcast=True)
+    with board.get_lock(), numbers.get_lock(), circles.get_lock():
+        board.value = data["board"]
+        numbers.value = data["numbers"]
+        circles.value = data["circles"]
+        emit("board", {"board": board.value, "numbers": numbers.value,
+             "circles": circles.value}, broadcast=True)
+
 
 @socketio.on("server-load")
 def loadPuzzle():
-    global board
-    global numbers
-    global circles
-    board = open("board.txt").read()
-    numbers = eval(open("numbers.txt").read())
-    circles = eval(open("circles.txt").read())
-    emit("board", {"board": board, "numbers": numbers, "circles": circles}, broadcast=True)
+    with board.get_lock(), numbers.get_lock(), circles.get_lock():
+        board.value = open("board.txt").read()
+        numbers.value = eval(open("numbers.txt").read())
+        circles.value = eval(open("circles.txt").read())
+        emit("board", {"board": board.value, "numbers": numbers.value,
+             "circles": circles.value}, broadcast=True)
+
 
 @socketio.on("server-store")
 def storePuzzle():
-    global board
-    global numbers
-    with open("board.txt", "w") as wr:
-        wr.write(board)
-    with open("numbers.txt", "w") as wr:
-        wr.write(str(numbers))
-    with open("circles.txt", "w") as wr:
-        wr.write(str(circles))
+    with board.get_lock(), numbers.get_lock(), circles.get_lock():
+        with open("board.txt", "w") as wr:
+            wr.write(board.value)
+        with open("numbers.txt", "w") as wr:
+            wr.write(str(numbers.value))
+        with open("circles.txt", "w") as wr:
+            wr.write(str(circles.value))
+
 
 if __name__ == '__main__':
     socketio.run(app)
